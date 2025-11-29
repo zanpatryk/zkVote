ad702abf67bbd4836b4d59377b6f1ebc06a97d35
mikolajed <mikolajedjed@gmail.com>
2025-11-29 23:04:27 +0100

    test: setup ui unit testing

---
+++ frontend/jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  dir: './',
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/pages/(.*)$': '<rootDir>/src/pages/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.js',
+    '!src/**/*.stories.{js,jsx,ts,tsx}',
+    '!src/pages/_app.js',
+    '!src/pages/_document.js',
+  ],
+  coverageReporters: ['text', 'html'],
+}
+
+module.exports = createJestConfig(customJestConfig)
