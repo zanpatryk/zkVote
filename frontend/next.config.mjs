9ed6ee1ade821cdc7f7d929450e72b8ca9727d0c
mikolajed <mikolajedjed@gmail.com>
2025-11-29 19:33:20 +0100

    fix: next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   webpack: (config) => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding', '@gemini-wallet/core', 'porto', 'porto/internal')
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+    }
     return config
   },
 };
